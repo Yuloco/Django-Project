8d118150e40411762de490f4cae0502e7d66d68b
Yuloco <yuloco@yahoo.com>
2022-06-18 19:36:45 +0100

    creating a blog content

--- I4G0129585ZP/blog/models.py
+++ I4G0129585ZP/blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-User=get_user_model()
+
 # Create your models here.
-class Post(models.Model):
-	title=models.CharField(max_length=200)
-    text=models.TextField()
-    author=models.ForeignKey(User, on_delete=models.CASCADE)
-	created_date=models.DateTimeField()
-	published_date= models.DateTimeField()
+class post(models.Model):
+    Text=models.TextField()
+    Title=models.CharField(max_length=200)
+    Author=models.ForeignKey(get_user_model)
+    Created_date=models.DateTimeField()
+    Publish_date=models.DateTimeField()
