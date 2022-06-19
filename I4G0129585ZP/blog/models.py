4aca4332a86c3e609459330c815b5a11828e4ace
Yuloco <yuloco@yahoo.com>
2022-06-19 17:12:24 +0100

    complete migrations for blog app

--- I4G0129585ZP/blog/models.py
+++ I4G0129585ZP/blog/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+user=get_user_model()
 
 # Create your models here.
 class post(models.Model):
     Text=models.TextField()
     Title=models.CharField(max_length=200)
-    Author=models.ForeignKey(get_user_model)
+    Author=models.ForeignKey(user, on_delete=models.CASCADE)
     Created_date=models.DateTimeField()
     Publish_date=models.DateTimeField()
+    def __str__(self):
+        return self.title
